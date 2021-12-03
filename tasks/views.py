729b4d021324abec3efe5bdd234e472b40c26d4a
GLeviosa <igigouz@gmail.com>
2021-12-03 20:08:36 -0300

    functions added

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
+from tasks.models import Task
+from django.core import serializers
+from django.forms.models import model_to_dict
+from rest_framework import generics
+from rest_framework.response import Response
+from tasks.serializers import TaskSerializer
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 
 
-# Create your views here.
+@api_view(["GET"])
+def taskGet(request):
+    data = TaskSerializer(Task.objects.all(), many=True)
+    return JsonResponse(data.data, safe=False)
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
+@api_view(["POST"])
+def taskPost(request):
+    data = JSONParser().parse(request)
+    serializer = TaskSerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=200)
+    return JsonResponse(serializer.errors, status=400)
+
+
+@api_view(["DELETE"])
+def taskDelete(request):
+    Task.objects.all().delete()
+    return HttpResponse("Todas as Tasks foram deletadas")
